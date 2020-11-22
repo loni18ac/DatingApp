fcad5c5d7c04fe63e2ab4ea822f51e3d1fcea0fb
loni18ac <70534823+loni18ac@users.noreply.github.com>
2020-11-22 16:53:14 +0100

    h

--- Server/server.js
+++ Server/server.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 })
 
 
-//login controller nedenfor
+/*//login controller nedenfor
 function loginController(req, res) {
     //Normalt vil man kigge om password og brugernavn stemmer, men det springer vi over
     //normalt vil man gemme secret key et andet sted. 
@@ -28,6 +28,6 @@ function loginController(req, res) {
     res.json({
         token: token
     })
-}
 
-module.exports = loginController
+
+module.exports = loginController*/
