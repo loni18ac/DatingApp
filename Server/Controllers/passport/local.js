2b8f7dffb8d679f5797495f5e58045f85653ef27
loni18ac <70534823+loni18ac@users.noreply.github.com>
2020-11-30 12:46:34 +0100

    bootstrap

--- Server/Controllers/passport/local.js
+++ Server/Controllers/passport/local.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../Models/User')
+const User = require('../Models/User.js')
 
 passport.serializeUser((user, done) => {
     done(null,user.id);
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 //Vi importerer express
 const app = express();
 //server sættes til at kalde på express
-const port = 6000;
+const port = 7000;
 //vi vælger port 4000, vi kunne også vælge fx 5000
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+
 const bodyParser = require('body-parser');
 //use bodyparser as middleware. Vi bruger to properties af body parser: URL encoded og json method
 const mongoose = require('mongoose');
 //vi loader mongoose module vba. require method og vi assigner den som et variabel kaldet mongoose
-const Keys = require('./database/keys')
+const Keys = require('./database/keys');
 //const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+/*const cookieParser = require('cookie-parser');
+const session = require('express-session');*/
 //vi loader express session module med require og assigner den til session
 
 app.use(bodyParser.urlencoded({extended:false})) //vi vil kun have form data
@@ -26,18 +29,25 @@ mongoose.connect(Keys.MongoDB, { useUnifiedTopology: true }).then(() => {
     console.log(err);
 })
 //vi sætter op view engine med to argumenter: navnet og det view den skal require
-app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.engine('handlebars', exphbs({handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 app.set('view engine', 'handlebars');
 
-
+app.get('/', (req,res) => {
+    res.render('home');
+});
+app.get('/createAccount', (req,res) => {
+    res.render('createAccount', {
+        title: 'Contact'
+    });
+});
 // Load models
 //brugeren er logget ind så længe sessionen varer vba. cookie id
-app.use(cookieParser);
+/*app.use(cookieParser);
 app.use(session({
    secret: 'mySecret',
    resave: true,
    saveUninitialized: true 
-}));
+}));*/
 /*app.use(passport.initialize());
 app.use(passport.session());
 
@@ -56,7 +66,7 @@ const User = require('./Server/Models/User.js');
 
 app.use('/routes/router.js', router);*/
 
-/*pp.use(flash());
+/*app.use(flash());
 app.use((req,res, next) =>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
@@ -75,13 +85,9 @@ app.use((req,res, next) =>{
 */
 
 
-app.get('/', (req,res) => {
-    res.render('home');
-});
 
-app.get('/createAccount', (req,res) => {
-    res.render('createAccount');
-});
+
+
 
 app.post('/createAccountHere', (req,res) => {
     console.log(req.body); //body/formen er objektet, som vi får ind. Vi skal installere body.parser, for at få det som JSON format
@@ -150,7 +156,7 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 })
 app.listen(port, () => {
- console.log(`Server running on http://localhost:${port}`)})
+ console.log(`Server running on http://localhost:${port}`)});
 
 /*
 /login controller nedenfor
